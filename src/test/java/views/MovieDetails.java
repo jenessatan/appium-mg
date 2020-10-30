87b5ce65c15ff7896cd131b651e727cfda6d7b93
jenessa <jenessaxtan@gmail.com>
2020-10-29 20:46:37 -0700

    start adding elements to movie details

--- src/test/java/views/MovieDetails.java
+++ src/test/java/views/MovieDetails.java
@@ -1,4 +1,41 @@
 package views;
 
-public class MovieDetails {
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+
+public class MovieDetails extends PageObject{
+    public MovieDetails(AndroidDriver<AndroidElement> driver) {
+        super(driver);
+    }
+
+    @AndroidFindBy(xpath="//android.widget.ImageButton[@content-desc='Navigate up']")
+    private AndroidElement backButton;
+
+    @AndroidFindBy(xpath = "//android.widget.ImageButton[@resource-id='com.esoxjem.movieguide:id/favorite']")
+    private AndroidElement favouriteButton;
+
+    @AndroidFindBy(xpath = "//android.widget.ImageView[@resource-id='android.widget.ImageView']")
+    private AndroidElement bannerImage;
+
+    @AndroidFindBy(xpath="//android.widget.TextView[@resource-id='com.esoxjem.movieguide:id/movie_name']")
+    private AndroidElement movieTitle;
+
+    @AndroidFindBy(xpath="//android.widget.TextView[@resource-id='com.esoxjem.movieguide:id/movie_year']")
+    private AndroidElement releaseDate;
+
+    @AndroidFindBy(xpath="//android.widget.TextView[@resource-id='com.esoxjem.movieguide:id/movie_rating']")
+    private AndroidElement rating;
+
+    @AndroidFindBy(xpath="//android.widget.TextView[@text='Summary']")
+    private AndroidElement summaryLabel;
+
+    @AndroidFindBy(xpath="//android.widget.TextView[@resource-id='com.esoxjem.movieguide:id/movie_description']")
+    private AndroidElement movieDescription;
+
+    @AndroidFindBy(xpath="//android.widget.TextView[@resource-id='com.esoxjem.movieguide:id/trailers_label']")
+    private AndroidElement trailerLabel;
+
+
+
 }
